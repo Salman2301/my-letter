983e44fd28f272370c676dcc2081523ee0bcade8
Salman <salmanhammed77@gmail.com>
2024-01-01 01:13:50 +0530

    feat: skeleton layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <script>
-    // cni-alert
-  import * as Alert from "$lib/components/ui/alert"
+  
 </script>
-<Alert.Root>
-  <Alert.Title>Heads up!</Alert.Title>
-  <Alert.Description>
-    You can add components to your app using the cli.
-  </Alert.Description>
-</Alert.Root>
+
+<h1>Test</h1>
