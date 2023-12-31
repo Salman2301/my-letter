dc888f45173d2404d966a5a0c2b79c673bbbbdcf
Salman <salmanhammed77@gmail.com>
2023-12-31 23:19:58 +0530

    init: supabase connected

---
+++ src/lib/db.ts
@@ -0,0 +1,11 @@
+
+import { env } from '$env/dynamic/public';
+import { createClient } from '@supabase/supabase-js';
+import type { Database } from '../database.types';
+
+const supabase = createClient<Database>(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_KEY ?? '')
+
+
+export function init() {
+  console.log('Supabase initialized')
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.pcss';
+	import {init} from "$lib/db";
+
+	init();
 </script>
 
 <slot />
