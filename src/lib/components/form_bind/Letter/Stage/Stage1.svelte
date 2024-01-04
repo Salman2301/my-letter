d32f74b12a387db6ee171b0bcae613d4021e1771
Salman <salmanhammed77@gmail.com>
2024-01-04 09:03:56 +0530

    fix: update letter title options

--- src/lib/components/form_bind/Letter/Stage/Stage1.svelte
+++ src/lib/components/form_bind/Letter/Stage/Stage1.svelte
@@ -6,6 +6,8 @@
   export let body: string = "";
   export let title: string = "";
 
+  let letterTitleOptions: string[] = ['Acceptance Letter', 'Acknowledgment Letter', 'Agreement Letter', 'Announcement Letter', 'Apology Letter', 'Appeal Letter', 'Application Letter', 'Appointment Letter', 'Appreciation Letter', 'Authorization Letter', 'Birthday Letter', 'Cancellation Letter', 'Certification Letter', 'Charity Letter', 'Claim Letter', 'Collection Letter', 'Complaint Letter', 'Condolence Letter', 'Confirmation Letter', 'Congratulations Letter', 'Cover Letter', 'Credit Letter', 'Criticism Letter', 'Dismissal Letter', 'Dispute Letter', 'Donation Letter', 'Endorsement Letter', 'Farewell Letter', 'Follow Up Letter', 'Friendship Letter', 'Fundraising Letter', 'Grievance Letter', 'Inquiry Letter', 'Internship Letter', 'Interview Letter', 'Introduction Letter', 'Invitation Letter', 'Marketing Letter', 'Order Letter', 'Permission Letter', 'Promotion Letter', 'Proposal Letter', 'Recommendation Letter', 'Reference Letter', 'Request Letter', 'Resignation Letter', 'Retirement Letter', 'Romantic Letter', 'Sales Letter', 'Scholarship Letter', 'Sorry Letter', 'Sponsorship Letter', 'Termination Letter', 'Thank You Letter', 'Transfer Letter', 'Warning Letter', 'Welcome Letter']
+
 </script>
 
 <div class="section section-1">
@@ -16,7 +18,7 @@
       label="Title"
       maxInputWidth={true}
       theme="light"
-      options={["My Apology letter"]}
+      options={letterTitleOptions}
     />
   </div>
   
