98e6bc3e51722a807ca38338c2b8e535184d1e79
Salman <salmanhammed77@gmail.com>
2024-01-06 14:54:01 +0530

    refactor: move button color into a component

--- src/lib/components/template/CustomizationSetting/Background.svelte
+++ src/lib/components/template/CustomizationSetting/Background.svelte
@@ -1,47 +1,24 @@
 <script lang="ts">
 	import TitleCustomization from "./TitleCustomization.svelte";
 	import InputUploadBtn from "$lib/components/form/InputUploadBtn.svelte";
-	import InputColor from "$lib/components/form/InputColor.svelte";
-	import clickOutside from "$lib/action/click-outside";
+	import ButtonColor from "$lib/components/form/ButtonColor.svelte";
 
   function handleUploadFile(ev: CustomEvent<{url: string}>) {
     const { url } = ev.detail;
     console.log(" upload file url : ", url)
   }
 
-  let showColorPicker: boolean = false;
+  let colorValue: string = "#000";
 
-  async function toggleColorPicker() {
-    showColorPicker = !showColorPicker;
-  }
 </script>
 
 <div class="background-container">
   <TitleCustomization content="Background" />
-  <div class="buttons" use:clickOutside={{ cb: ()=>showColorPicker=false}}>
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div
-      role="button"
-      tabindex="0"
-      class="btn"
-      on:click|self={toggleColorPicker}
-      aria-label="Choose a background color"
-    >
-      <InputColor
-        bind:isOpen={showColorPicker}
-        disableClickOutside={true}
-      />
-
-      <!-- svelte-ignore a11y-click-events-have-key-events -->
-      <div
-        class="label" 
-        on:click|self={toggleColorPicker}
-        role="button"
-        tabindex="0"
-        >
-        Select a color
-      </div>
-    </div>
+  <div class="buttons" >
+    <ButtonColor 
+      label="Select a background"
+      bind:colorValue={colorValue}
+    />
     <InputUploadBtn
       on:upload={handleUploadFile}
       bucket="user_background"
@@ -50,16 +27,9 @@
   </div>
 </div>
 
-
 <style lang="postcss">
   .buttons {
     @apply flex flex-col gap-2;
-  }
-  .btn {
-    @apply flex justify-center gap-2;
-    @apply bg-background text-secondary-foreground;
-    @apply py-2 px-4;
-    
   }
 
 </style>
