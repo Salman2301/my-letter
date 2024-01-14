71bab4fdbb95fa83267cfe489f79492797b12226
Salman <salmanhammed77@gmail.com>
2024-01-15 01:55:22 +0530

    feat: add delete button my contact page

--- src/routes/app/my-contacts/+page.svelte
+++ src/routes/app/my-contacts/+page.svelte
@@ -7,22 +7,41 @@
 
 	import type { Tables } from '$lib/database.types';
 	import type { Columns } from '$lib/components/table_bind/table.types';
+	import { Trash2Icon } from 'lucide-svelte';
 
 	let isLoading: boolean = true;
 	let contacts: Tables<'contact'>[] = [];
+	export let checkedRowState: boolean[] = [];
+	export let checkedRowIndices: number[] = [];
+
+	$: showDeleted = checkedRowState.filter(Boolean).length > 0;
 
 	const MAX_CONTACTS = 10;
 	$: disabledContact = contacts.length > MAX_CONTACTS;
 
 	onMount(async () => {
+		fetchData();
+	});
+
+	async function fetchData() {
+		isLoading = true;
+		
 		let { data, error } = await supabase.from('contact').select('*');
 		if (error) {
 			console.error(error);
 		}
 
 		contacts = (data || []) as Tables<'contact'>[];
+		checkedRowState = contacts.map(() => false);
 		isLoading = false;
-	});
+	}
+
+	async function handleDeleteSelected( ){
+		const ids = checkedRowIndices.map((index) => contacts[index].id);
+		const { data, error } = await supabase.from('contact').delete().in("id", ids);
+		
+		fetchData();
+	}
 
 	const columns: Columns<keyof Tables<'contact'>> = [
 		{
@@ -61,6 +80,18 @@
 		</div>
 	{:else}
 		<div class="action">
+			{#if showDeleted}
+				<button
+					class="btn-container btn-delete"
+					on:click={handleDeleteSelected}
+				>
+					<div class="btn">
+						<div class="icon"><Trash2Icon size="22" /></div>
+						<div class="label">Delete ({checkedRowIndices.length})</div>
+					</div>
+				</button>
+			{/if}
+
 			<a
 				href={disabledContact ? '#' : '/app/new-contact'}
 				class="btn-container"
@@ -70,7 +101,7 @@
 					<div class="icon"><PlusIcon size="22" /></div>
 					<div class="label">New contact ( {contacts.length}/10 )</div>
 				</div>
-			</a>
+			</a>		
 
 		</div>
 		<Card>
@@ -82,6 +113,8 @@
 					<Table 
 						columns={columns}
 						rows={contacts}
+						bind:checkedRowState
+						bind:checkedRowIndices
 					/>
 
 			{/if}
@@ -96,9 +129,10 @@
 	}
 
 	.action {
-		@apply flex justify-end;
+		@apply flex justify-end gap-2;
 		@apply my-2;
 	}
+
 
 	.btn-container {
 		@apply bg-primary text-secondary-foreground;
@@ -117,6 +151,11 @@
 		@apply bg-accent;
 		text-decoration: none; /* Remove underline */
 		cursor: not-allowed; /* Change cursor style */
+	}
+	.btn-delete {
+		@apply bg-destructive;
+		@apply hover:bg-destructive;
+		@apply hover:underline;
 	}
 
 	.image {
